1c6c74351a65236417058b15fb4b1ede8f118790
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-17 11:21:07 -0300

    Listando clientes e enderecos

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,16 @@ Route::get('/clientes', function () {
         echo "<p> ID: " . $c->id . "</p>";
         echo "<p> Nome: " . $c->nome . "</p>";
         echo "<p> Telefone: " . $c->telefone . "</p>";
+        echo "<hr>";
 
-        echo "<p> rua : " . $c->endereco->rua . "</p>";
-        echo "<p> numero : " .  $c->endereco->numero . "</p>";
-        echo "<p> bairro : " . $c->endereco->bairro . "</p>";
-        echo "<p> Cidade : " . $c->endereco->cidade . "</p>";
-        echo "<p> UF : " . $c->endereco->uf . "</p>";
-        echo "<p> CEP : " . $c->endereco->cep . "</p>";
+        // echo "<p> rua : " . $c->endereco->rua . "</p>";
+        // echo "<p> numero : " .  $c->endereco->numero . "</p>";
+        // echo "<p> bairro : " . $c->endereco->bairro . "</p>";
+        // echo "<p> Cidade : " . $c->endereco->cidade . "</p>";
+        // echo "<p> UF : " . $c->endereco->uf . "</p>";
+        // echo "<p> CEP : " . $c->endereco->cep . "</p>";
 
-        echo "</br>";
+        // echo "</br>";
     }
 });
 
@@ -36,12 +37,12 @@ Route::get('/enderecos', function () {
     $enderecos = Endereco::all();
     foreach($enderecos as $end){
         echo "<p> ID cliente : " . $end->cliente_id . "</p>";
-        echo "<p> rua : " . $end->rua . "</p>";
-        echo "<p> numero : " . $end->numero . "</p>";
-        echo "<p> bairro : " . $end->bairro . "</p>";
+        echo "<p> Rua : " . $end->rua . "</p>";
+        echo "<p> Numero : " . $end->numero . "</p>";
+        echo "<p> Bairro : " . $end->bairro . "</p>";
         echo "<p> Cidade : " . $end->cidade . "</p>";
         echo "<p> UF : " . $end->uf . "</p>";
         echo "<p> CEP : " . $end->cep . "</p>";
-        echo "</br>";
+        echo "<hr>";
     }
 });
